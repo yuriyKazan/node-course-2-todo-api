96b71e8551f51e967df4c3fb3f3bf4a1bb791bee
yuriy.kazan <yuriy_kazan@edvantis.com>
2017-02-24 18:07:33 +0200

    Add POST /todos route and refactor mongoose.

---
+++ playground/test-mongoose.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/testMonsoose');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we\'re connected!');
+
+    var kittySchema = mongoose.Schema({
+        name: String
+    });
+
+    kittySchema.methods.speak = function () {
+    var greeting = this.name
+        ? "Meow name is " + this.name
+        : "I don't have a name";
+    console.log(greeting);
+    }
+
+    var Kitten = mongoose.model('Kitten', kittySchema);
+
+    var silence = new Kitten({ name: 'Silence' });
+    console.log(silence.name); // 'Silence' 
+
+    var fluffy = new Kitten({ name: 'fluffy' });
+    fluffy.speak(); // "Meow name is fluffy"
+
+    fluffy.save(function (err, fluffy) {
+        if (err) return console.error(err);
+        fluffy.speak();
+    });
+});
+
+
