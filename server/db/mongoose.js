f1566f3a07dcc195d50d891d6f28d5ebadfdd85a
yuriy.kazan <yuriy_kazan@edvantis.com>
2017-03-02 20:26:12 +0200

    Fix connect.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//'mongodb://admin:123456@ds115110.mlab.com:15110/mydb'
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//'mongodb://admin:123456@ds115110.mlab.com:15110/mydb'  process.env.MONGODB_URI process.env.PORT y = x > 0 ? 1 : -1;
+mongoose.connect(process.env.PORT ? 'mongodb://admin:123456@ds115110.mlab.com:15110/mydb' : 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
